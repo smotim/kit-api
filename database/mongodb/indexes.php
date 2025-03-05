7323478bb0805979636ca4680af22dff0cbc71da
Timofey <timofeykasmolin@gmail.com>
2025-03-05 11:00:41 +0500

    fix: code cleanup

--- database/mongodb/indexes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    \App\Models\Terminal::class => [
-        ['key' => ['name' => 'text', 'address' => 'text']],
-        ['key' => ['terminal_id' => 1], 'unique' => true],
-        ['key' => ['city_id' => 1]],
-    ],
-];
